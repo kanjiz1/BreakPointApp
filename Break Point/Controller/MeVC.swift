95561ea6dfcde758c3900e8a81f7b1988790a7cf
Kanjiz1 <kanjis007@gmail.com>
2018-07-06 20:42:41 +0300

    Created Profile Tab

---
+++ Break Point/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  Break Point
+//
+//  Created by Oforkanji Odekpe on 7/6/18.
+//  Copyright © 2018 Oforkanji Odekpe. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutButtonWasPressed(_ sender: Any) {
+    }
+}
