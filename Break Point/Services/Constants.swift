79b32143fea56c5b8067df33d4931654e80a93a6
Kanjiz1 <kanjis007@gmail.com>
2018-07-06 18:48:47 +0300

    Creating InsetTextField and ShadowView Subclasses

---
+++ Break Point/Services/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Break Point
+//
+//  Created by Oforkanji Odekpe on 7/6/18.
+//  Copyright © 2018 Oforkanji Odekpe. All rights reserved.
+//
+
+import Foundation
+
+let AUTH_VC = "AuthVC"
+let LOGIN_VC = "LoginVC"
---
+++ Break Point/View/ShadowView.swift
@@ -0,0 +1,20 @@
+//
+//  ShadowView.swift
+//  Break Point
+//
+//  Created by Oforkanji Odekpe on 7/6/18.
+//  Copyright © 2018 Oforkanji Odekpe. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.opacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = UIColor.black.cgColor
+        super.awakeFromNib()
+    }
+    
+}
