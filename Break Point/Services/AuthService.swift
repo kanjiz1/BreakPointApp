a79c47d206e47c5c73c83624bd10675aacfe695b
Kanjiz1 <kanjis007@gmail.com>
2018-07-06 19:10:27 +0300

    Added functions to create and login user

---
+++ Break Point/Services/AuthService.swift
@@ -0,0 +1,41 @@
+//
+//  AuthService.swift
+//  Break Point
+//
+//  Created by Oforkanji Odekpe on 7/6/18.
+//  Copyright © 2018 Oforkanji Odekpe. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) ->()){
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.user.providerID, "email": user.user.email]
+            
+            DataService.instance.createDBUSer(uid: user.user.uid, userData: userData)
+            
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginCreationComplete: @escaping (_ status: Bool, _ error: Error?) ->()){
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else{
+                loginCreationComplete(false, error)
+                return
+            }
+            loginCreationComplete(true, nil)
+        }
+    }
+}
