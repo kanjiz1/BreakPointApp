5d2db20f6c5affd02b12747c5ccdc26e54014843
Kanjiz1 <kanjis007@gmail.com>
2018-08-11 15:18:28 -0500

    Getting all groups from Firebase

--- Break Point/Controller/GroupsVC.swift
+++ Break Point/Controller/GroupsVC.swift
@@ -11,10 +11,23 @@ import UIKit
 class GroupsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var groupsArray = [Group]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.tableView.reloadData()
+            }
+        }
     }
 }
 
@@ -24,14 +37,14 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard  let cell = tableView.dequeueReusableCell(withIdentifier: GROUP_CELL, for: indexPath) as? GroupCell else {
             return UITableViewCell()}
         
-        cell.configureCell(title: "Big head", description:"This is my first group", memberCount: 4)
+        cell.configureCell(title: groupsArray[indexPath.row].groupTitle, description:groupsArray[indexPath.row].groupDescription, memberCount: groupsArray[indexPath.row].memberCount)
         
         return cell
     }
